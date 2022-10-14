6112c83b8db2e26592f49bbc25e99f4322eebd91
DanielTverd <daniel1902elle@gmail.com>
2022-10-14 16:48:27 +0300

    new info for lesson

--- LESSON 18 JSON and mutation/index.js
+++ LESSON 18 JSON and mutation/index.js
@@ -35,4 +35,84 @@ JSON - JavaScript object notation
 // const postStringified = JSON.stringify(post)
 // JSON.parse(postStringified)
 -----------------------------------------------------
+Мутация в JavaScript
+
+Как копируются значения примитивных типов ? 
+Пример ниже :
+
+const a = 10
+let b = a
+b = 30
+console.log(a) - 10
+console.log(b) - 30
+
+Значения ссылочного типа
+Пример : 
+
+const person = {
+    name: 'Bob',
+    age: 21
+} 
+
+person.age = 22                         произошла мутация объекта , на который ссылается переменная "person"
+person.isAdult = true                   копирование по значению (copy by value)
+
+console.log(person.age) - 22
+console.log(person.isAdult) - true
+
+
+МУТИРОВАНИЕ КОПИЙ 
+ПРИМЕР НИЖЕ:
+
+const person = {
+    name: 'Bob',
+    age: 25
+} 
+const person2 = person                      копирование ссылки (copy by reference)
+
+person2.age = 26
+person2.isAdult = true
+
+console.log(person.age) - 26
+console.log(person.isAdult) - true
+-----------------------------------------------------
+КАК ИЗБЕЖАТЬ МУТАЦИИ ?
+объснение на примере :
+Пример №1
+const person = {
+    name: 'Bob',
+    age: 25
+} 
+
+const persone2 = Object.assing({}, person)
+
+persone2.age = 26
+
+console.log(person2.age) - 26
+console.log(persone.age) - 25
+
+Пример №2
+const person = {
+    name: 'Bob',
+    age: 25
+} 
+const persone2 = { ...person }      этот синтаксис ... (оператор разделения объекта на свойства)
+
+persone2.name = 'Alice'
+
+console.log(person2.name) - Alice
+console.log(persone.name) - Bob
+
+Пример №3
+Полное избежение мутации
+const person = {
+    name: 'Bob',
+    age: 25
+} 
+const persone2 = JSON.parse(JSON.stringify(persone))        конвертация в объект , после конвертация в строку !!!
+
+persone2.name = 'Alice'
+
+console.log(person2.name) - Alice
+console.log(persone.name) - Bob
 */
