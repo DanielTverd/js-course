f0e20f4101b707e219c2e1741b3c6a4fba3ebd39
DanielTverd <daniel1902elle@gmail.com>
2022-10-27 19:26:07 +0300

    add new lesson

--- LESSON 45 string concatenation/index.js
+++ LESSON 45 string concatenation/index.js
@@ -1,3 +1,31 @@
 /*
-КОНКАТЕНАЦИЯ СТРОК
+КОНКАТЕНАЦИЯ СТРОК (соединение строк)
+
+ОПЕРАТОР + ДЛЯ КОНКАТЕНАЦИИ СТРОК :
+
+'Hello ' + 'World!' //'Hello World!'
+
+переменные в конкатенации строк , пример ниже
+
+// const hello = 'Hello'
+// const world = 'World'
+
+// const greeting = hello + ' ' + world
+
+ШАБЛОННЫЕ СТРОКИ :
+
+const hello = 'Hello'
+const world = 'World'
+
+const greeting = `${hello} ${world}`
+
+
+// const myName = 'Привет, меня зовут'
+// const firstName = 'ДАНИЭЛЬ'
+// const myCity = ', я живу в городе'
+// const realCity = 'ОДЕССА'
+
+// const fullLetter = `${myName} ${firstName} ${myCity} ${realCity}`
+
+// console.log(fullLetter)
 */
