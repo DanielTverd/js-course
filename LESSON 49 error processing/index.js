335829555172d14b8c2fe9b1f4cacd139a33d440
DanielTverd <daniel1902elle@gmail.com>
2022-11-01 21:02:17 +0200

    add new lesson

---
+++ LESSON 49 error processing/index.js
@@ -0,0 +1,13 @@
+/*
+Обработка ошибок
+
+Что происходит в случае ошибок ?
+
+const fnWithError = () => {
+    throw new Error('Some error')           
+}
+fnWithError()                   выполнение кода остановится после появления Uncaught ошибки
+console.log('Continue...')
+
+
+*/
