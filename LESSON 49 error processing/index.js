1fa98e53aafd18eb93ad962d4523030887c73e82
DanielTverd <daniel1902elle@gmail.com>
2022-11-01 21:11:54 +0200

    add new commit

--- LESSON 49 error processing/index.js
+++ LESSON 49 error processing/index.js
@@ -9,5 +9,26 @@ const fnWithError = () => {
 fnWithError()                   выполнение кода остановится после появления Uncaught ошибки
 console.log('Continue...')
 
+РЕШЕНИЕ ТАКИХ ПРОБЛЕМ ? НИЖЕ
 
+TRY/CATCH
+
+try{
+    // выполнение блока кода
+} catch (error) {
+    // этот блок выполняется в случае возникновения ошибок в блоке try
+}
+
+Пример
+
+const fnWithError = () => {
+    throw new Error('Some error')           
+}
+try{
+    fnWithError()
+} catch (error) {
+    console.error(error)
+    console.log(error.message)
+}
+console.log('Continue...')
 */
