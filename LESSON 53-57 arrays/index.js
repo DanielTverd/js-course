83ed87d0e4bbf6d6db27f56098b141a24f899252
DanielTverd <daniel1902elle@gmail.com>
2022-11-09 20:21:57 +0200

    add new info

--- LESSON 53-57 arrays/index.js
+++ LESSON 53-57 arrays/index.js
@@ -63,4 +63,19 @@ console.log(myArray[1]) //true
 
 console.log(myArray.length) //3
 
+Длина массива:
+
+const myArray = [1, 2, 3, 4]
+console.log(myArray) //[1, 2, 3, 4]
+console.log(myArray.length) //4
+
+myArray[2] = 'abc'
+
+console.log(myArray) //[1, 2, 'abc', 4]
+console.log(myArray[2]) //'abc'
+
+myArray[4] = true
+
+console.log(myArray) //[1, 2, 'abc', 4, true]
+console.log(myArray.length) //5
 */
