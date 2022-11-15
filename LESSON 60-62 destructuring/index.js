c85a70f5c59bc9b03f7454df403168048db2d8cb
DanielTverd <daniel1902elle@gmail.com>
2022-11-15 19:49:29 +0200

    add new info to the lesson

--- LESSON 60-62 destructuring/index.js
+++ LESSON 60-62 destructuring/index.js
@@ -1,3 +1,22 @@
 /*
+ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТОВ
+пример:
+
+const userProfile = {
+    name: 'Daniel',
+    commentsQty: 20,
+    hasSignedAgreement: false
+}
+
+const { name, commentsQty } = userProfile
+const { hasSignedAgreement } = userProfile
+
+console.log(name)//Daniel
+console.log(commentsQty)//20
+-----------------------------------------------------
+ДЕСТРУКТУРИЗАЦИЯ МАССИВОВ
+пример:
+
+
 
 */
