205cd8d58a68af8f83c2ac0c225fb2b347b214eb
DanielTverd <daniel1902elle@gmail.com>
2022-11-15 20:00:03 +0200

    add new info to the lesson

--- LESSON 60-62 destructuring/index.js
+++ LESSON 60-62 destructuring/index.js
@@ -8,15 +8,36 @@ const userProfile = {
     hasSignedAgreement: false
 }
 
-const { name, commentsQty } = userProfile
+const { name, commentsQty } = userProfile       объявление новых переменных и присваивание значений на основе значений свойств объекта
 const { hasSignedAgreement } = userProfile
 
 console.log(name)//Daniel
 console.log(commentsQty)//20
------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------
 ДЕСТРУКТУРИЗАЦИЯ МАССИВОВ
 пример:
 
+const fruits = ['Apple', 'Banana']      
+const [fruitOne, fruitTwo] = fruits     объявление новых переменных и присваивание значений на основе эдементов массива
 
+console.log(fruitOne)//Apple
+console.log(fruitTwo)//Banana
+-------------------------------------------------------------------------------------------------------------------------------
+ДЕСТРУКТУРИЗАЦИЯ ПАРАМЕТРОВ ФУНКЦИИ
+пример:
 
+const userProfile = {
+    name: 'Daniel',
+    commentsQty: 20,
+    hasSignedAgreement: false
+}
+
+const userInfo = ({ name, commentsQty}) => {        деструктуризация параметров функции
+    if (!commentsQty) {
+        return `User ${name} has ${commentsQty} comments`
+    }
+    return `User ${name} has ${commentsQty} comments`
+}
+
+userInfo(userProfile) // User Daniel has 20 comments
 */
