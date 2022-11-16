7817480b18db4db162a830dec136791bb5a636dc
DanielTverd <daniel1902elle@gmail.com>
2022-11-16 21:35:14 +0200

    add new info

--- LESSON 63-65 conditional instructions/index.js
+++ LESSON 63-65 conditional instructions/index.js
@@ -19,4 +19,13 @@ if (val > 5) {
 }
 console.log(val)//30
 
+Пример if с оператором отрицания
+
+const person = {
+    age: 20
+}
+if (!person.name) {             !undefined === true
+    console.log('Имя не указано')
+    //Другие действия в случае, если свойства "name" у объекта "person" нету
+}
 */
