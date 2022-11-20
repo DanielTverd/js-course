a89ee8b4bfe422d594042a2ec19a84361c2d0745
DanielTverd <daniel1902elle@gmail.com>
2022-11-20 17:49:13 +0200

    add new info

--- LESSON 63-65 conditional instructions/index.js
+++ LESSON 63-65 conditional instructions/index.js
@@ -76,17 +76,6 @@ if (age >= 18) {
     console.log('Is child')
 }
 
-const age = 25
-
-if (age > 18) {
-    console.log('Is adult')
-}
-if (age >= 12) {
-    console.log('Is teenager')
-} 
-if (age < 12) {
-    console.log('Is child')
-}
 
 ------------------
 
