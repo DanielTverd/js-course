9f38166cde7e162713b787730d5a70f98c1682f9
DanielTverd <daniel1902elle@gmail.com>
2022-11-24 20:00:48 +0200

    add new info to the lesson

--- LESSON 67-68 switch and ternary operator/index.js
+++ LESSON 67-68 switch and ternary operator/index.js
@@ -69,4 +69,13 @@ value1 && value2
     ? myFunction1(value1, value2)
     : myFanction2()
 
+Пример 3:
+
+let value = 11
+console.log(value >= 0 ? value : -value) //11
+
+value = -5
+const res = value >= 0 ? value : -value
+console.log(res) //5
+
 */
