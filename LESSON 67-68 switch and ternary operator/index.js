7226136ae61940a172b8f989261bc316f566f772
DanielTverd <daniel1902elle@gmail.com>
2022-11-24 19:36:07 +0200

    add new info to the lesson

--- LESSON 67-68 switch and ternary operator/index.js
+++ LESSON 67-68 switch and ternary operator/index.js
@@ -13,6 +13,24 @@ switch (Выражение) {
         //Действие по умолчанию
 }
 
+Пример:
+
+const month = 2
+
+switch (month) {
+    case 12:
+        console.log('Декабрь')
+        break
+    case 1:
+        console.log('Январь')
+        break
+    case 2:
+        console.log('Февраль')
+        break
+    default:
+        console.log('Это не зимний месяц')
+}
+
 ------------------------------------
 Тернарный оператор
 */
