7dc9e58606a73e245c7904e22e37ec00f1cec5d4
DanielTverd <daniel1902elle@gmail.com>
2022-11-24 19:56:55 +0200

    add new info

--- LESSON 67-68 switch and ternary operator/index.js
+++ LESSON 67-68 switch and ternary operator/index.js
@@ -52,4 +52,21 @@ switch (month) {
 ? Выражение 1
 : Выражение 2
 
+Пример 1:
+
+const value = 11
+
+value 
+    ? console.log('Условие истинно')
+    : console.log('Условие ложно')
+
+Пример 2:
+
+const value1 = 11
+const value2 = 25
+
+value1 && value2
+    ? myFunction1(value1, value2)
+    : myFanction2()
+
 */
