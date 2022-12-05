0c6c57de3c1fdfe9fd294314f6152861ba797688
DanielTverd <daniel1902elle@gmail.com>
2022-12-05 13:41:42 +0200

    add new info

--- LESSON 78-83 modules and exports/index.js
+++ LESSON 78-83 modules and exports/index.js
@@ -5,5 +5,19 @@
 Модули позволяют избегать дублирования блоков кода!
 
 EXPORT/IMPORT синтаксис появился в ES6!
+
+примеры:
+
+moduleOne.js
+
+const myName = () => {
+    console.log('Daniel')
+}
+export default myName
+
+moduleTwo.js
+
+import printMyName from './moduleOne.js'        //Названия переменных могут не совпадать
+printMyName() //Daniel
 -------------------------------------------------
 */
