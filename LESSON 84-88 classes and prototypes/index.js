9e4ad6f2a31d3a147ab899d33cbb9e0a8dce02c1
DanielTverd <daniel1902elle@gmail.com>
2023-01-04 10:38:41 +0200

    add new info

--- LESSON 84-88 classes and prototypes/index.js
+++ LESSON 84-88 classes and prototypes/index.js
@@ -24,6 +24,18 @@ class Comment {
 }
 -----------------------------------------
 Создание экземпляров класса
+
+class Comment {
+    constructor(text) {
+        this.text = text  
+        this.votesQty = 0
+    }
+    upvote() {
+        this.votesQty += 1
+    }
+}
+
+const firstComment = new Comment('First comment')       вызывается функция constructor (создание экземпляра)
 -----------------------------------------
 -----------------------------------------
 -----------------------------------------
